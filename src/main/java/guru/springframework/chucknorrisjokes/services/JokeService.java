bfad013e84c2b26a00696eaa4475c5a1bf01f920
Haire Brandon <Brandon.Haire@ngc.com>
2023-10-13 16:12:06 -0400

    created joke service. Closes #3

---
+++ src/main/java/guru/springframework/chucknorrisjokes/services/JokeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.chucknorrisjokes.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/guru/springframework/chucknorrisjokes/services/JokeServiceImpl.java
@@ -0,0 +1,21 @@
+package guru.springframework.chucknorrisjokes.services;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotesl) {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+
+}
